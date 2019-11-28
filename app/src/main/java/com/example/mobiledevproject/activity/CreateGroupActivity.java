5d0a070fa429471181976927e93e37ece0b839fd
Fy <fuyu0824@pku.edu.cn>
2019-11-28 15:53:04 +0800

    home page 0.1

---
+++ app/src/main/java/com/example/mobiledevproject/activity/CreateGroupActivity.java
@@ -0,0 +1,16 @@
+package com.example.mobiledevproject.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mobiledevproject.R;
+
+public class CreateGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_group);
+    }
+}
