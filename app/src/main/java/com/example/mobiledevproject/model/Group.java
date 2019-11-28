749789d9a7094ceb46bb3a206e32d4307a8f52dd
Fy <fuyu0824@pku.edu.cn>
2019-11-28 14:51:49 +0800

    init commit

---
+++ app/src/main/java/com/example/mobiledevproject/model/Group.java
@@ -0,0 +1,11 @@
+package com.example.mobiledevproject.model;
+
+import java.util.List;
+
+public class Group {
+
+    private List<User> memberList;
+    private int groupId;
+    private String userName;
+
+}
---
+++ app/src/main/java/com/example/mobiledevproject/model/User.java
@@ -0,0 +1,8 @@
+package com.example.mobiledevproject.model;
+
+public class User {
+
+    private int userId;
+    private String userName;
+
+}
